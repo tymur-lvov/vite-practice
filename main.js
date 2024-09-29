af190423b5b4ba0682c35821b4da951e7c90b999
tymur-lvov <155083435+tymur-lvov@users.noreply.github.com>
2024-09-29 17:23:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 import './style.css';
 
 const getMarkupPartials = async (path) => {
-  const data = await fetch(path);
-
-  return data.text();
+  try {
+    const data = await fetch(path);
+  
+    return data.text();
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 const concatMarkupPartials = async () => {
-  const markup = [];
-
-  markup.push(await getMarkupPartials('/vite-practice/src/partials/html/partial1'));
-  markup.push(await getMarkupPartials('/src/partials/html/partial2'));
-  markup.push(await getMarkupPartials('/src/partials/html/partial3'));
-
-  return markup.join('');
+  try {
+    const markup = [];
+  
+    markup.push(await getMarkupPartials('/vite-practice/src/partials/html/partial1'));
+    markup.push(await getMarkupPartials('/src/partials/html/partial2'));
+    markup.push(await getMarkupPartials('/src/partials/html/partial3'));
+  
+    return markup.join('');    
+  } catch (error) {
+    console.log(error.message);
+  }
 };
-
-console.log(await concatMarkupPartials())
 
 document.querySelector('#app').innerHTML = await concatMarkupPartials();
