de071d8175b0695668dcc8721e34de495b12af5c
tymur-lvov <pmtl753@gmail.com>
2024-09-29 16:38:00 +0300

    Initialize project 1.0

---
+++ main.js
@@ -0,0 +1,19 @@
+import './style.css';
+
+const getMarkupPartials = async (path) => {
+  const data = await fetch(path);
+
+  return data.text();
+};
+
+const concatMarkupPartials = async () => {
+  const markup = [];
+
+  markup.push(await getMarkupPartials('./src/partials/html/partial1'));
+  markup.push(await getMarkupPartials('./src/partials/html/partial2'));
+  markup.push(await getMarkupPartials('./src/partials/html/partial3'));
+
+  return markup.join('');
+};
+
+document.querySelector('#app').innerHTML = await concatMarkupPartials();
